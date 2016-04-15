eb50e0556edfd260d3def9e405f56de9df786afe
Ubuntu <ubuntu@ip-172-31-25-233.eu-west-1.compute.internal>
2016-04-15 19:17:35 +0000

    .

--- generate_certificate.sh
+++ generate_certificate.sh
@@ -1,1 +1,1 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout jupyter.pem -out jupyter.pem
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/jupyter.pem -out ~/jupyter.pem
